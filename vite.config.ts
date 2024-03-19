a606c8505085cb79631e0311e1fdfacc89f3fdb8
Adrian Zinko <zinko.adrian00@gmail.com>
2024-03-19 11:23:41 +0100

    update base config, add sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import packageJson from './package.json';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    sourcemap: true,
+  },
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [splitVendorChunkPlugin(), react()],
 });
