8c15faefe0cc396dffc93385019b2c52bad359f8
Adrian Zinko <zinko.adrian00@gmail.com>
2024-03-27 12:28:02 +0100

    Add terraform/imports.tf and comment out aws_s3_bucket_policy in terraform/s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,21 +10,22 @@ resource "aws_s3_bucket_ownership_controls" "ofed-bucket-ownership-controls" {
   }
 }
 
-resource "aws_s3_bucket_policy" "ofe_bucket_policy" {
-  bucket = aws_s3_bucket.ofe_bucket.id
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "PublicReadGetObject",
-        "Effect" : "Allow",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::ofd-bucket/*"
-      }
-    ]
-  })
-}
+# resource "aws_s3_bucket_policy" "ofed_bucket_policy" {
+#   bucket = aws_s3_bucket.ofed-bucket.id
+
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Sid" : "PublicReadGetObject",
+#         "Effect" : "Allow",
+#         "Principal" : "*",
+#         "Action" : "s3:GetObject",
+#         "Resource" : "arn:aws:s3:::ofed-bucket/*"
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_s3_bucket_website_configuration" "ofed-bucket-website" {
   bucket = aws_s3_bucket.ofed-bucket.id
