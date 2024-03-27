66fa2e3e519f7cdcf2ebd74e0517954d25980816
Adrian Zinko <zinko.adrian00@gmail.com>
2024-03-27 12:48:19 +0100

    Update S3 bucket name and add bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,34 +1,34 @@
-resource "aws_s3_bucket" "ofed-bucket" {
+resource "aws_s3_bucket" "ofed_bucket" {
   bucket = "zinko-terraform-ofed-bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "ofed-bucket-ownership-controls" {
-  bucket = aws_s3_bucket.ofed-bucket.id
+  bucket = aws_s3_bucket.ofed_bucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-# resource "aws_s3_bucket_policy" "ofed_bucket_policy" {
-#   bucket = aws_s3_bucket.ofed-bucket.id
+resource "aws_s3_bucket_policy" "ofed_bucket_policy" {
+  bucket = aws_s3_bucket.ofed_bucket.id
 
-#   policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Sid" : "PublicReadGetObject",
-#         "Effect" : "Allow",
-#         "Principal" : "*",
-#         "Action" : "s3:GetObject",
-#         "Resource" : "arn:aws:s3:::ofed-bucket/*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::zinko-terraform-ofed-bucket/*"
+      }
+    ]
+  })
+}
 
 resource "aws_s3_bucket_website_configuration" "ofed-bucket-website" {
-  bucket = aws_s3_bucket.ofed-bucket.id
+  bucket = aws_s3_bucket.ofed_bucket.id
 
   index_document {
     suffix = "index.html"
