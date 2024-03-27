63644e3d846026b1fae882cb6a758a01143091e1
Adrian Zinko <zinko.adrian00@gmail.com>
2024-03-27 13:01:29 +0100

    Remove commented out code for aws_s3_bucket_policy in s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,22 +10,22 @@ resource "aws_s3_bucket_ownership_controls" "ofed-bucket-ownership-controls" {
   }
 }
 
-resource "aws_s3_bucket_policy" "ofed_bucket_policy" {
-  bucket = aws_s3_bucket.ofed_bucket.id
+# resource "aws_s3_bucket_policy" "ofed_bucket_policy" {
+#   bucket = aws_s3_bucket.ofed_bucket.id
 
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "PublicReadGetObject",
-        "Effect" : "Allow",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::zinko-terraform-ofed-bucket/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Sid" : "PublicReadGetObject",
+#         "Effect" : "Allow",
+#         "Principal" : "*",
+#         "Action" : "s3:GetObject",
+#         "Resource" : "arn:aws:s3:::zinko-terraform-ofed-bucket/*"
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_s3_bucket_website_configuration" "ofed-bucket-website" {
   bucket = aws_s3_bucket.ofed_bucket.id
